195b6d8ea1d2092b5f0f0056c201210eb579c9d9
Ashlie Gray <ashliejanegray@gmail.com>
2021-01-01 17:34:32 +1000

    drag wasn't updating with song time, now fully functional

--- src/components/Player.js
+++ src/components/Player.js
@@ -16,22 +16,25 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
             setIsPlaying(!isPlaying)
         }
     }
+
     const timeUpdateHandler = (e) => {
         const current = e.target.currentTime
         const duration = e.target.duration
         setSongInfo({...songInfo, currentTime: current, duration})
-
     }
+
     const getTime = (time) => {
         return (
             Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
         )
     }
+
     const dragHandler = (e) => {
-        audioRef.current.currentTime = e.target.value
-        setSongInfo({...songInfo , currentTime: e.target.value})
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({...songInfo, currentTime: e.target.value})
     }
-    const [songInfo, setSongInfo] =useState({
+
+    const [songInfo, setSongInfo] = useState({
         currentTime: null,
         duration: null,
     })
@@ -40,7 +43,12 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         <div className='player'>
             <div className='time-control'>
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input min={0} max={songInfo.duration} value={songInfo.currentTime} type='range' />
+                <input 
+                    min={0} 
+                    max={songInfo.duration}
+                    onChange={dragHandler} 
+                    value={songInfo.currentTime} 
+                    type='range' />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className='play-control'>
@@ -50,8 +58,7 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
             </div>
             <audio 
                 onTimeUpdate={timeUpdateHandler} 
-                onLoadedMetadata={timeUpdateHandler}
-                onChange={dragHandler} 
+                onLoadedMetadata={timeUpdateHandler} 
                 ref={audioRef} 
                 src={currentSong.audio}>
             </audio>
