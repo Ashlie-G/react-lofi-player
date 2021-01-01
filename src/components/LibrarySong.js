bb8716af4c4920cbaef2807cd628ba0fc47d8a23
Ashlie Gray <ashliejanegray@gmail.com>
2021-01-01 19:47:41 +1000

    can no select and play songs from library on side of the window

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({song, songs, setCurrentSong, id}) => {
+
+    const songSelectHandler = () => {
+        // the below is unnessisary as we alread have the song by passing song as a prop
+        // const selectedSong = songs.filter((state) => state.id === id)
+        // setCurrentSong(selectedSong[0])
+        setCurrentSong(song)
+    }
+
     return (
-        <div className='library-song'>
+        <div onClick={songSelectHandler} className='library-song'>
             <img src={song.cover} alt='album cover'></img>
             <div className='song-description'>
             <h3>{song.name}</h3>
