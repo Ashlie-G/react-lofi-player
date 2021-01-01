2f50abc7e86e1f2f286730f85adf32273528811e
Ashlie Gray <ashliejanegray@gmail.com>
2021-01-01 16:48:45 +1000

    audio added, now can play and pause onclick of play icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Player from './components/Player'
 import Song from './components/Song'
 import data from './util'
 import './styles/app.scss'
 
 function App() {
+//state
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
+
   return (
     <div className="App">
-      <Song/>
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+      setIsPlaying={setIsPlaying}
+      isPlaying={isPlaying} 
+      currentSong={currentSong} 
+      />
     </div>
   );
 }
